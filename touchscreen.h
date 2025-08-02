7e67d9c5208ccda9fac11b6234d6d409a30f3c48
intrexx mervore <146873484+numbpill3d@users.noreply.github.com>
2025-08-02 12:08:36 -0400

    Create touchscreen.h

---
+++ touchscreen.h
@@ -0,0 +1,57 @@
+/*
+ * DriftRiff Mini - Touchscreen Handler Header
+ */
+
+#ifndef TOUCHSCREEN_H
+#define TOUCHSCREEN_H
+
+#include <TouchScreen.h>
+#include <Adafruit_ILI9341.h>
+
+// Touch calibration values (adjust based on your screen)
+#define TS_MINX 150
+#define TS_MINY 120
+#define TS_MAXX 920
+#define TS_MAXY 940
+
+// Touch action types
+enum TouchActionType {
+  TOUCH_NONE,
+  TOUCH_GRID,
+  TOUCH_BPM_UP,
+  TOUCH_BPM_DOWN,
+  TOUCH_PLAY_PAUSE,
+  TOUCH_CLEAR_TRACK,
+  TOUCH_CLEAR_ALL
+};
+
+struct TouchAction {
+  TouchActionType type;
+  int track;
+  int step;
+  int x;
+  int y;
+};
+
+class TouchHandler {
+private:
+  TouchScreen* touchScreen;
+  Adafruit_ILI9341* display;
+  
+  unsigned long lastTouchTime;
+  int lastTouchX, lastTouchY;
+  
+  // Coordinate transformation
+  int mapTouchX(int rawX);
+  int mapTouchY(int rawY);
+  
+public:
+  TouchHandler();
+  
+  void init(TouchScreen* ts, Adafruit_ILI9341* tft);
+  TSPoint getTouch();
+  TouchAction processTouchInput(int rawX, int rawY);
+  
+  void calibrate();
+  bool isValidTouch(TSPoint p);
+};
